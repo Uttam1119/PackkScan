a277df77097d4f72543c2e0782c8f0f55efdb6ab
Uttam <kumaruttam519@gmail.com>
2025-10-04 19:34:25 +0530

    dealt with some issues

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
+import { UserPreferencesProvider } from './contexts/UserPreferencesContext';
+import { FoodHistoryProvider } from './contexts/FoodHistoryContext';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
